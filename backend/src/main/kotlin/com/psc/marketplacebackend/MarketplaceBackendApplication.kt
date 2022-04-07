f0e996444b7b7559fd44716086ef0f050955dfea
maquison <maquisonfelipe@gmail.com>
2022-04-07 11:15:54 -0300

    first commit

---
+++ backend/src/main/kotlin/com/psc/marketplacebackend/MarketplaceBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.psc.marketplacebackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MarketplaceBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<MarketplaceBackendApplication>(*args)
+}
