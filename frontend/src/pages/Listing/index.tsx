a15ba71d88798c448accf34f06c0b14e01828279
juarezmeneses <juarezlmfilho@gmail.com>
2022-05-31 19:12:15 -0300

    Added useState and useEffect in the item listing page

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,19 +1,28 @@
 import axios from "axios";
 import ItemCard from "components/ItemCard";
 import Pagination from "components/Pagination";
+import { useEffect, useState } from "react";
+import { ItemPage } from "types/item";
 import { BASE_URL } from "utils/requests";
 
 function Listing() {
 
-    // FORMA ERRADA
-    axios.get(`${BASE_URL}/api/items-page?size=12&page=1&sort=id`)
-        .then(response => {
-            console.log(response.data);
-        });
+    const [pageNumber, setPageNumber] = useState(0);
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/api/items-page?size=12&page=0&sort=id`)
+            .then(response => {
+                const data = response.data as ItemPage;
+                console.log(response.data);
+                setPageNumber(data.number);
+            });
+    }, []);
 
     return (
         <>
+            <p>{pageNumber}</p>
             <Pagination />
+
             <div className="container">
                 <div className="row">
                     <div className="col-sm-6 col-lg-4 col-xl-3 mb-3" >
